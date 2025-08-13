6b47b2f685ea05580ab1e823a8ab66221b6e927c
Nangamso <ST10477430@rcconect.edu.za>
2025-08-13 13:04:07 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+ ;   public static void main(String[] args) {
+        String[] Areas = {"JHB", "DBN", "CTN", "PE"};
+        String[] Monthly = {"JAN", "FEB", "MAR"};
+        int[][] MakeOvers = {{128, 135, 139}, {155, 129, 175}, {129, 130, 185}, {195, 155, 221}};
+
+        System.out.println("***************************** ");
+        System.out.println("MonthlySpeed");
+        System.out.println("******************************** ");
+
+
+        System.out.println();
+
+         for (int j = 0; j < Monthly.length; j++) {
+            ;
+        }
+
+
+        System.out.println();
+
+        for (int j = 0; j < Monthly.length; j++) {
+         int total = Integer.parseInt(("MakeOvers +[i][1] i++"));
+        }
+
+
+        System.out.println();
+
+        for (int j = 0; j < Monthly.length; j++) {
+            ;
+        }
+        System.out.println("MonthlySpeed: + ");
+        System.out.println("Monthly.length");
+        System.out.println("SPEEDING FINES STATISTICS");
+
+    }
+
+
+}
